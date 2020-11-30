218d426feec483e5addb1c1ed229a27acce3661c
positiveko <edie_ko@naver.com>
2020-11-30 19:36:56 +0900

    [Fix] 폴더명 수정

---
+++ src/Components/Property/Property.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PropertyDetail = (props) => (
+      <h1>Property</h1>
+  );
+
+export default PropertyDetail;
---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Main.scss';
+
+
+const Main = (props) => <div>Main</div>;
+
+export default Main;
